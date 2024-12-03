06f5967f3e6757d2b682ae17287acc5729b215f4
Anung B Ariwibowo <barliant@gmail.com>
2024-12-03 14:48:02 +0700

    Buat outline tugas di script template app.py

--- app.py
+++ app.py
@@ -3,3 +3,25 @@ import pandas as pd
 import plotly.express as px
 
 st.title("Kelompok Bunga Abadi")
+
+st.write("# Tugas Kelompok PasmingBased")
+
+st.write("## Pendahuluan")
+st.write("Tuliskan di bagian ini latar belakang data apa yang dipilih, mengapa kelompok memilih data ini, dsb.")
+
+st.write("## Deskripsi Data")
+st.write("Tuliskan di bagian ini deskripsi tentang data yang digunakan.")
+
+st.write("## Visualisasi")
+st.write("Buat visualisasi yang menurut kelompok kalian perlu ditampilkan.")
+st.write("Gunakan juga elemen-elemen interaktif `streamlit`.")
+
+st.write("## Analisis")
+st.write("Buat analisis sederhana dari visualisasi data yang muncul di bagian sebelumnya.")
+
+st.write("## Kesimpulan")
+st.write("Tuliskan butir-butir kesimpulan dari analisis.")
+
+st.write("## Referensi / Daftar Pustaka")
+st.write("Tuliskan di bagian ini referensi yang digunakan dalam proyek kelompok ini, misalnya sumber data, makalah ilmiah, dsb.")
+
