d169d9abfa51bec3ecda8dacbe50f704e1cddd09
Maharani Ardina <maharaniardina9@gmail.com>
2024-12-12 15:38:52 +0700

    menambahkan deskripsi data

--- app.py
+++ app.py
@@ -2,16 +2,15 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 
-st.title("Analisis Pengaruh Investasi dan Tingkat Kemiskinan Terhadap Pertumbuhan Ekonomi")
+st.title("Kelompok Bunga Abadi")
 
-st.write("# Tugas Kelompok Bunga Abadi")
+st.write("# Tugas Kelompok PasmingBased")
 
 st.write("## Pendahuluan")
-st.write('''Indonesia sebagai negara berkembang memiliki tujuan untuk meningkatkan kesejahteraan rakyatnya melalui pertumbuhan ekonomi yang stabil dan berkelanjutan. Namun, kemiskinan masih menjadi salah satu masalah utama yang dihadapi oleh Indonesia. Menurut data dari Badan Pusat Statistik (BPS), pada tahun 2022, tingkat kemiskinan di Indonesia masih sebesar 9,22%. Kemiskinan ini dapat mempengaruhi pertumbuhan ekonomi Indonesia karena orang miskin tidak memiliki kemampuan untuk berinvestasi dan meningkatkan produksi. 
-Pertumbuhan ekonomi Indonesia telah mengalami peningkatan yang signifikan dalam beberapa tahun terakhir. Namun, pertumbuhan ekonomi yang tidak merata dan tidak adil masih menjadi salah satu masalah utama yang dihadapi oleh Indonesia. Menurut data dari Bank Indonesia, pada tahun 2022, pertumbuhan ekonomi Indonesia sebesar 5,17%. Pertumbuhan ekonomi ini dapat dipengaruhi oleh beberapa faktor, termasuk investasi dan tingkat kemiskinan.''')
+st.write("Tuliskan di bagian ini latar belakang data apa yang dipilih, mengapa kelompok memilih data ini, dsb.")
 
 st.write("## Deskripsi Data")
-st.write("Tuliskan di bagian ini deskripsi tentang data yang digunakan.")
+st.write('''Pertumbuhan ekonomi adalah sebuah proses dari perubahan kondisi perekonomian yang terjadi di suatu negara secara berkesinambungan untuk menuju keadaan yang dinilai lebih baik selama jangka waktu tertentu. ''')
 
 st.write("## Visualisasi")
 st.write("Buat visualisasi yang menurut kelompok kalian perlu ditampilkan.")
