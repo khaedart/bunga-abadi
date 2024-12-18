0b6f5a12290a4cb2658f8601b6a768f63f51c1f2
Khaedar tamim <khaedar223@gmail.com>
2024-12-18 15:07:40 +0700

    membuat visualisasi

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import plotly.express as px
 
 st.title("Analisis Saham Perusahaan di Indonesia")
 
-st.write("# Tugas Kelompok PasmingBased")
+st.write("# Tugas Kelompok bunga-abadi")
 
 st.write("## Pendahuluan")
 st.write('''Kami memilih menganalisis dataset saham Indonesia karena pasar modal di Indonesia menunjukkan potensi yang besar untuk pertumbuhan dan perkembangan ekonomi. Dalam beberapa tahun terakhir, minat masyarakat terhadap investasi saham semakin meningkat, seiring dengan kemudahan akses informasi dan teknologi yang memfasilitasi transaksi di pasar modal. Dengan lebih dari 700 perusahaan terdaftar di Bursa Efek Indonesia, terdapat beragam pilihan investasi yang dapat dieksplorasi. Melalui analisis dataset saham ini dapat memahami dinamika pergerakan harga, volume perdagangan, serta faktor-faktor yang mempengaruhi kinerja saham, seperti kondisi ekonomi makro, kebijakan pemerintah, dan sentimen pasar.
@@ -19,6 +19,7 @@ st.write("## Visualisasi")
 import streamlit as st
 import yfinance as yf
 import plotly.express as px
+import seaborn as sns
 
 df = pd.read_csv('DaftarSaham.csv')
 
@@ -34,6 +35,87 @@ kamus_ticker = {
     'ACES': 'Ace Hardware Indonesia Tbk'
 }
 
+import numpy as np
+
+import os
+for dirname,_, filenames in os.walK('/kaggle/input'):
+    for filename in filenames:
+        print(os.path.join(dirname, filename))
+
+import warnings
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+import plotly.subplots as ms
+from plotly.offline import iplot
+warnings.filterwarnings("ignore")
+
+df=pd.read_csv('/kaggle/input/ihsgstockdata/minutes/AALI.csv')
+df = df.loc[df['timestamp']>='2022-04-01'].reset_index(drop=True)
+
+WINDOW = 60
+df['sma'] = df['close'].rolling(WINDOW).mean()
+df['std'] = df['close'].rolling(WINDOW).std(ddof = 0)
+display(df)
+
+fig = make_subplots(specs=[[{"secondary_y": True}]])
+
+# include candlestick with rangeselector
+fig.add_trace(go.Candlestick(x=df['timestamp'],
+                open=df['open'], high=df['high'],
+                low=df['low'], close=df['close'], name='AALI'),
+               secondary_y=False)
+# Moving Average
+fig.add_trace(go.Scatter(x = df['timestamp'],
+                         y = df['sma'],
+                         line_color = 'black',
+                         name = 'Simple Moving Average'))
+
+# Upper Bound
+fig.add_trace(go.Scatter(x = df['timestamp'],
+                         y = df['sma'] + (df['std'] * 2),
+                         line_color = 'gray',
+                         line = {'dash': 'dash'},
+                         name = 'Bollinger Band',
+                         opacity = 0.5))
+
+# Lower Bound
+fig.add_trace(go.Scatter(x = df['timestamp'],
+                         y = df['sma'] - (df['std'] * 2),
+                         line_color = 'gray',
+                         line = {'dash': 'dash'},
+                         showlegend=False,
+                         opacity = 0.5))
+
+# include a go.Bar trace for volumes
+fig.add_trace(go.Bar(x=df['timestamp'], y=df['volume'], name='Volume'),
+               secondary_y=True)
+fig.update_xaxes(
+    rangeslider_visible=True,
+    rangebreaks=[
+            dict(bounds=["sat", "mon"]),  
+            dict(bounds=[16, 9], pattern="hour")
+            ],
+    rangeselector=dict(
+        buttons=list([
+            dict(count=1, label="1h", step="hour", stepmode="backward"),
+            dict(count=5, label="5h", step="hour", stepmode="backward"),
+            dict(count=1, label="1d", step="day", stepmode="backward"),
+            dict(count=7, label="1w", step="day", stepmode="backward"),
+            dict(step="all")
+        ])
+    )
+)
+fig.layout.yaxis2.showgrid=False
+# Add figure title
+fig.update_layout(
+    title_text="Data Saham AALI"
+)
+# Set x-axis title
+fig.update_xaxes(title_text="Date")
+# Set y-axes titles
+fig.update_yaxes(title_text="<b>Price</b>", secondary_y=False)
+fig.update_yaxes(title_text="<b>Volume</b>", secondary_y=True)
+fig.show()
 # Select box for ticker symbol with a unique key
 tickerSymbol = st.selectbox(
     'Silakan pilih kode perusahaan',
